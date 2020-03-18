fdab664ff957cb08953c62a4be1cbf7c032ab967
Mlie <emipa606@users.noreply.github.com>
2020-03-18 20:46:28 +0100

    First publish

---
+++ Source/NoMoreAutoroof/AutoBuildRoofAreaSetter_AutoBuildRoofAreaSetterTick_First.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HarmonyLib;
+using Verse;
+
+namespace NoMoreAutoroof
+{
+    // Token: 0x02000003 RID: 3
+    [HarmonyPatch(typeof(AutoBuildRoofAreaSetter), "TryGenerateAreaNow", new Type[] { typeof(Room) })]
+    internal class AutoBuildRoofAreaSetter_AutoBuildRoofAreaSetterTick_First
+    {
+        // Token: 0x06000002 RID: 2 RVA: 0x00002080 File Offset: 0x00000280
+        [HarmonyPrefix]
+        private static bool TryGenerateAreaNow_Fix()
+        {
+            return false;
+        }
+    }
+}
---
+++ Source/NoMoreAutoroof/NoMoreAutoroof.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace NoMoreAutoroof
+{
+	// Token: 0x02000002 RID: 2
+	[StaticConstructorOnStartup]
+	internal class NoMoreAutoroof : Mod
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public NoMoreAutoroof(ModContentPack content) : base(content)
+		{
+			var harmonyInstance = new Harmony("fed1s.rimworld.NoMoreAutoroof");
+			harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
