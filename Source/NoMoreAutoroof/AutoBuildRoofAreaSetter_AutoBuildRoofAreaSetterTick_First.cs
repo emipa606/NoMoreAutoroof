dad17bc4e48dc3e02fc75ca2d8be53d0dfd5ac19
emipa606 <emil.offermo@litet.net>
2021-07-10 22:12:21 +0200

    Mod updated for 1.3 and passed autotests

--- Source/NoMoreAutoroof/AutoBuildRoofAreaSetter_AutoBuildRoofAreaSetterTick_First.cs
+++ Source/NoMoreAutoroof/AutoBuildRoofAreaSetter_AutoBuildRoofAreaSetterTick_First.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using HarmonyLib;
+﻿using HarmonyLib;
 using Verse;
 
 namespace NoMoreAutoroof
 {
     // Token: 0x02000003 RID: 3
-    [HarmonyPatch(typeof(AutoBuildRoofAreaSetter), "TryGenerateAreaNow", new Type[] { typeof(Room) })]
+    [HarmonyPatch(typeof(AutoBuildRoofAreaSetter), "TryGenerateAreaNow", typeof(Room))]
     internal class AutoBuildRoofAreaSetter_AutoBuildRoofAreaSetterTick_First
     {
         // Token: 0x06000002 RID: 2 RVA: 0x00002080 File Offset: 0x00000280
